06ac729aeac481bc8751ef9c2a78a0e9e1336c9f
yrsci <31885317+yrsci@users.noreply.github.com>
2017-10-01 19:06:55 +0100

    Update gif_generator.py

--- gif_generator.py
+++ gif_generator.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  1 18:03:55 2017
+
+@author: Yv
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,18 +25,32 @@ def func(func_name, x, *args, **kwargs):
         quad_yval = a*(x**2) + b*x + c
         return quad_yval
     
+    def expon(x, A=1.0):
+        exp_yval = A * (np.e**x)
+        return exp_yval
+    
     if func_name == "line":
         return line(x, *args, **kwargs)
     if func_name == "quad":
         return quad(x, *args, **kwargs)
+    if func_name == "exp":
+        return expon(x, *args, **kwargs)
     else:
         return None
 
 # Generate an array of x values:
-xvals = np.arange(0.0, 20.0, 1.0)
+xvals = np.arange(-10.0, 20.0, 1.0)
 yvals = []
 
+# Change the desired type of function to generate by attributing either "line"
+# or "quad" to func_type:
 func_type = "line"
+
+function_names = {
+        "quad" : "a quadratic function",
+        "line" : "a linear function",
+        "exp" : "an exponential function"
+        }
 
 filenames = []
 
@@ -40,11 +59,14 @@ for i, x in enumerate(xvals):
     y = func(func_type, x)
     yvals.append(y)
     plt.plot(xvals[:i+1], yvals, "*")
-    plt.title("Generating a " + func_type + " using matplotlib")
+    plt.title("Generating " + function_names[func_type] + " using matplotlib")
     
     # Generate axis values based on the xvals array
     plt.xlim(xvals[0]-1,xvals[-1]+1)
-    plt.ylim(func(func_type, np.sort(abs(xvals))[0]-1), func(func_type, np.sort(abs(xvals))[-1]+1))
+    if func_type == "line":
+        plt.ylim(func(func_type,xvals[0]-1), func(func_type, xvals[-1]+1))
+    else:
+        plt.ylim(func(func_type, np.sort(abs(xvals))[0]-1), func(func_type, np.sort(abs(xvals))[-1]+1))
     
     # Save current plot as .png file (in the same directory) & append its name 
     # to a list of image filenames:
@@ -52,7 +74,7 @@ for i, x in enumerate(xvals):
     plt.savefig(filename)
     filenames.append(filename)
     
-    # Close the current plot before generating the next one (to save resources)
+    # Close the current plot before generating the next one (to save memory)
     plt.close()
 
 # Generate a .gif using the plots exported by matplotlib
